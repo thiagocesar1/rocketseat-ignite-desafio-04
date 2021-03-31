ebf90316cdab86f95a3d8b29cd5648f952d869f6
Thiago Alvares <thiago.cesaralvares@gmail.com>
2021-03-31 18:59:35 -0300

    Desafio finalizado.

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,17 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+
+      const users = this.showUserProfileUseCase.execute({ user_id });
+
+      return response.json(users);
+    } catch (err) {
+      return response.status(404).json({
+        error: err,
+      });
+    }
   }
 }
 
